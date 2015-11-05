308c6e2554d31b8162cf8e75d2f9f53a09822e31
jroland81093 <james.a.roland@gmail.com>
2015-11-05 15:26:18 -0800

    Added Constants.swift file for credentials and form methods

---
+++ Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  REST-sample
+//
+//  Created by James Roland on 11/5/15.
+//  Copyright © 2015 James Roland. All rights reserved.
+//
+
+import Foundation
+
+let GET = "GET"
+let POST = "POST"
+let PUT = "PUT"
+let PATCH = "PATCH"
+
+//OAuth 1.0
+let Yelp =
+[
+  "consumerKey": "***",
+  "consumerSecret": "***"
+]
+
+//OAuth 2.0
+let Github =
+[
+  "consumerKey": "***",
+  "consumerSecret": "***"
+]
+
+
+
